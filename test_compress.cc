bffaf70c2000b938a96a72c66d3f831c11e7ba25
Daniel Urbanowicz <daniel.urbanowicz@yahoo.co.uk>
2011-04-29 19:01:29 +0200

    remove comments

--- test_compress.cc
+++ test_compress.cc
@@ -1,8 +1,3 @@
-/////////////////////////////////////////////////
-// Program kompresuj¥cy pliki graficzne        //
-// o formacie *.mtv                            //
-/////////////////////////////////////////////////
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<ctype.h>
@@ -10,26 +5,26 @@
 
 int main(void)
 {
-	FILE *plikwe, *plikwy;		// wsazniki do plikow
-	char tab[9], k1[3], k2[3];  // tablice znakow
+	FILE *plikwe, *plikwy;		
+	char tab[9], k1[3], k2[3];
 	char znak, koniec;
-	char *t1, *t2;           	// wskazniki string'ow
-	int j, x, y, h;
-	int licznik1, ikonwersji;   //licznik pierwszych znakow
-	unsigned long i, ilosc, licznik_konw;	//licznik
-												//powtarzajacych sie znakow
+	char *t1, *t2;           	
+	int j, xres, yres, h;
+	int licznik1, ikonwersji;  
+	unsigned long i, ilosc, licznik_konw;	
+
   int licznik2;
 
 	ikonwersji = 8;
 	licznik_konw = 0;
 
-	if((plikwe = fopen("images/fisheye.mtv","r+b"))==NULL)  //otwarcie pliku wej.
+	if((plikwe = fopen("images/fisheye.mtv","r+b"))==NULL)  
 	{
 		printf("Blad otwarcia pliku wejsciowego !\n");
 		return 1;
 	}
 
-	if((plikwy = fopen("fisheye.k","w+b"))==NULL)  //otwarcie pliku wyj.
+	if((plikwy = fopen("fisheye.k","w+b"))==NULL) 
 	{
 		printf("Blad otwarcia pliku wyjsciowego !\n");
 		return 1;
@@ -38,24 +33,24 @@ int main(void)
 	for(i=0; i<5; i++)
 	{
 		tab[i] = getc(plikwe);
-		if((isdigit(tab[i]))==0)   //sprawdza czy znak jest cyfra
+		if((isdigit(tab[i]))==0) 
 		{
 			tab[i]=0;
 			break;
 		}
 	}
-	x = atoi(tab);  // x - rozdzielczosc
+	xres = atoi(tab);
 
 	for( i=0; i<5; i++)
 	{
 		tab[i] = getc(plikwe);
-		if((isdigit(tab[i]))==0)  //sprawdza czy znak jest cyfra
+		if((isdigit(tab[i]))==0) 
 		{
 			tab[i]=0;
 			break;
 		}
 	}
-	y = atoi(tab); // y - rozdzielczosc
+	yres = atoi(tab); 
 
 	fseek(plikwe, 0, SEEK_SET);
 	licznik1 = 0;
@@ -63,17 +58,17 @@ int main(void)
 	{
 		fread(&znak, 1, 1, plikwe);
 		fwrite(&znak, 1, 1, plikwy);
-		licznik1 = licznik1 + 1;		//licznik pierwszych znakow
+		licznik1 = licznik1 + 1;	
 	}
-//////////////////////////////////////////////////////////////////////////////
-	fseek(plikwy, licznik1+ikonwersji, SEEK_SET);	//ust. na zapis pikseli
-	fread(k1, 1, 3, plikwe);				// odczytanie pierwszego piksela
-	strncpy(k2, k1, 3);                     //i skopiowanie go
-	fseek(plikwe, licznik1 , SEEK_SET);     //powt¢rne ustaw. na tym pikselu
-	j = strncmp(k1, k2, 3);					//por¢wnanie pikseli (true)
+
+	fseek(plikwy, licznik1+ikonwersji, SEEK_SET);
+	fread(k1, 1, 3, plikwe);
+	strncpy(k2, k1, 3);     
+	fseek(plikwe, licznik1 , SEEK_SET);  
+	j = strncmp(k1, k2, 3);
 	licznik2 = 0;
 	i = 0;
-	ilosc = (long)x * (long)y;				//ilo˜† pikseli
+	ilosc = (long)xres * (long)yres;
 	while(i <= ilosc)
 	{
 		if(i!=ilosc)
@@ -81,17 +76,17 @@ int main(void)
 			fread(k1, 1, 3, plikwe);
 			j = strncmp(k1, k2, 3);
 		}
-		if(j != 0 || i == (ilosc))  // warunek sprawdzenia tych samych
-		{                           //pikseli i zapisu ostatnich pikseli(a)
+		if(j != 0 || i == (ilosc))
+		{
 			fwrite(k2, 1, 3, plikwy);
 			if(licznik2 == 0)
 			licznik2++;
 			fwrite(&licznik2, sizeof(licznik2), 1, plikwy);
 			licznik2 = 0;
 			strncpy(k2, k1, 3);
-			licznik_konw++;         //liczba r¢¾nych pikseli
+			licznik_konw++;     
 		}
-		licznik2 = licznik2 + 1;	//zliczanie tych samych pikseli
+		licznik2 = licznik2 + 1;	
 		i = i + 1;
 	}
 
@@ -102,7 +97,7 @@ for(i=0; i<9; i++)
 {
 	if(tab[i] != 0)
 	{
-		fwrite(&tab[i], 1, 1, plikwy); //zapis do pliku wyj. ilo˜† konwersji
+		fwrite(&tab[i], 1, 1, plikwy); 
 	}
 	else break;
 }
