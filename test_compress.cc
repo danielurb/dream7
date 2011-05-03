372e906893c0c86b4a787d12d9bd5b377cc66c09
Daniel Urbanowicz <daniel.urbanowicz@yahoo.co.uk>
2011-05-03 18:17:11 +0200

    reduce to main

--- test_compress.cc
+++ test_compress.cc
@@ -1,69 +1,9 @@
 #include <stdio.h>
-#include <string.h>
 
-//TODO: fix conversion bug (different pixels)
-//TODO: remove conversion count field from final format
-
-
-struct Pixel
-{
-  char r;
-  char g;
-  char b;
-};
-
-
-bool same_pixel(const Pixel &pixel, const Pixel &prev_pixel)
-{
-  return (0 == memcmp(&pixel, &prev_pixel, sizeof(Pixel)));
-}
-
-void read_pixel(FILE *input, Pixel &pixel)
-{
-  fread(&pixel, 1, sizeof(Pixel), input);
-}
-
-void copy_pixel(Pixel &dst_pixel, const Pixel &src_pixel)
-{
-  memcpy(&dst_pixel, &src_pixel, sizeof(Pixel));
-}
-
-void write_conversion(FILE *output, const Pixel &pixel, int repetition)
-{
-  fwrite(&pixel, 1, sizeof(Pixel), output);
-  fwrite(&repetition, sizeof(repetition), 1, output);
-}
+void compress(FILE *input, FILE *output);
 
 int main(void)
 {
-  long xres = 0;
-  long yres = 0;
-
-  fscanf(stdin, "%ld %ld\n", &xres, &yres);
-  fprintf(stdout, "%ld %ld\n", xres, yres);
-
-  fwrite("7840\x0\x0\x0\x0", 8, 1, stdout);
-
-  Pixel prev_pixel;
-  read_pixel(stdin, prev_pixel);
-  int repetitions = 1;
-
-  for (long i = 1; i < (xres * yres); i++)
-  {
-    Pixel pixel;
-    read_pixel(stdin, pixel);
-
-    if (!same_pixel(pixel, prev_pixel))
-    {
-      write_conversion(stdout, prev_pixel, repetitions);
-      repetitions = 0;
-      copy_pixel(prev_pixel, pixel);
-    }
-
-    repetitions++;
-  }
-
-  write_conversion(stdout, prev_pixel, repetitions);
-
+  compress(stdin, stdout);
   return 0;
 }
